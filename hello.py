3b094a385ff1e5d8f4ab2f627d1fdfce5d405a2c
att124 <daniel.fernandez@davinci.edu.ar>
2024-09-11 14:59:10 -0300

    Funciones

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 print("hola mundo")
 
 print("prueba de cambios")
+
+def mi_funcion():
+    print("hola soy una funcion")
+
+mi_funcion()
+
+def segunda_funcion():
+    
+    print("segunda funcion")
+
+segunda_funcion()
