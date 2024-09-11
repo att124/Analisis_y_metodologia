5b449f860e0c23ec16bd7f731ac0df2a4e28cdf3
att124 <daniel.fernandez@davinci.edu.ar>
2024-09-11 14:37:52 -0300

    versiones

--- hello.py
+++ hello.py
@@ -10,5 +10,16 @@ def mi_funcion():
 print(mi_funcion())
 
 
+def segundo_funcion():
+
+    print("segunda funcion")
+
+segundo_funcion()
+
+def tercera_funcion():
+
+    print("tercer funcion")
+
+tercera_funcion()
 
     
